5145de90d710c42a20e989154df716ce7d52dc55
Jordan Cannon <jordancannon@MBP13-OM141.local>
2019-01-18 11:54:16 -0700

    Deploy update

--- containers/workspace/config/init.sh
+++ containers/workspace/config/init.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-# Run composer install.
-/usr/local/bin/composer.phar install -d /var/www
+# Run composer install AND artisan migrate
+/usr/local/bin/composer.phar install -d /var/www && php artisan migrate:fresh --seed
 
 # This keeps the container running.
 /sbin/my_init
